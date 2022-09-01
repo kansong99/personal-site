e8d59b6496a7d53eb542eacad24e987027fd8bf7
Kofi <w.fifi99@gmail.com>
2022-09-01 00:58:13 -0700

    expose links

--- src/components/podcastLinks.jsx
+++ src/components/podcastLinks.jsx
@@ -11,26 +11,26 @@ import Overcast from "../assets/overcast.png";
 function PodcastLinks() {
   return (
     <div className="follow-us">
-      {/* <a className="btn-social btn-spotify" target="_blank" rel="noreferrer" href="https://open.spotify.com/">
+      <a className="btn-social btn-spotify" target="_blank" rel="noreferrer" href="https://open.spotify.com/show/2Yh9lE6Tf6qeOOIFcQ0ji0">
         <img alt="spotify button" src={PodcastSpotify} />
         <span>Spotify</span>
       </a>
-      <a className="btn-social btn-pocketcasts" target="_blank" rel="noreferrer" href="https://pca.st">
+      <a className="btn-social btn-pocketcasts" target="_blank" rel="noreferrer" href="https://pca.st/xoepj2r0">
         <img alt="pocket casts button" src={Pocketcasts} />
         <span>Pocketcasts</span>
       </a>
-      <a className="btn-social btn-itunes" target="_blank" rel="noreferrer" href="https://podcasts.apple.com/">
+      <a className="btn-social btn-itunes" target="_blank" rel="noreferrer" href="https://podcasts.apple.com/us/podcast/the-kd-pod/id1642757284">
         <img alt="itunes button" src={Itunes} />
         <span>iTunes</span>
       </a>
-      <a className="btn-social btn-googlepodcasts" target="_blank" rel="noreferrer" href="https://podcasts.google.com/">
+      {/* } <a className="btn-social btn-googlepodcasts" target="_blank" rel="noreferrer" href="https://podcasts.google.com/">
         <img alt="google podcast button" src={GooglePodcast} />
         <span>Google Podcast</span>
-      </a>
-      <a className="btn-social btn-overcasts" target="_blank" rel="noreferrer" href="https://overcast.fm/">
+  </a> */}
+      <a className="btn-social btn-overcasts" target="_blank" rel="noreferrer" href="https://overcast.fm/itunes1642757284">
         <img alt="overcast button" src={Overcast} />
         <span>Overcast</span>
-      </a> */}
+      </a>
       <a className="btn-social btn-rss" target="_blank" rel="noreferrer" href="https://kofiansong.com/podcast/rss.xml">
         <img alt="rss button" src={Rss} />
         <span>RSS</span>
